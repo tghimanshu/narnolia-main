0b48a1d522fa6eccd474c677c65c1ce8958cf9db
Himanshu <himanshugohil234@gmail.com>
2021-08-05 20:45:37 +0530

    made mobile responsive

--- client/src/pages/SinglePortfolio.jsx
+++ client/src/pages/SinglePortfolio.jsx
@@ -96,24 +96,24 @@ export function SinglePortFolio({ match }) {
                     {pf.Description}
                   </div>
                   <div className="badge bg-success mb-2">{pf.RiskCategory}</div>
-                  <div className="d-flex justify-content-between">
-                    <div>
+                  <div className="d-block d-md-flex justify-content-between">
+                    <div className="mb-1">
                       <small>Min. Investment Amount - </small>
                       <small className="fw-bold">{pf.MinInvestAmt}</small>
                     </div>
-                    <div>
+                    <div className="mb-1">
                       <small>Benchmark - </small>
                       <small className="fw-bold">{pf.BenchmarkReturn}</small>
                     </div>
-                    <div>
+                    <div className="mb-1">
                       <small>Time Horizon - </small>
                       <small className="fw-bold">{pf.Duration}</small>
                     </div>
-                    <div>
+                    <div className="mb-1">
                       <small>Expected CAGR - </small>
                       <small className="fw-bold">{pf.AnnualisedReturn}</small>
                     </div>
-                    <div>
+                    <div className="mb-1">
                       <small>No. of Stocks - </small>
                       <small className="fw-bold">{pf.MinSIPAmt}</small>
                     </div>
@@ -123,16 +123,16 @@ export function SinglePortFolio({ match }) {
             </div>
           </section>
           <div className="description_section">
-            <div className="mt-4">
+            <div className="mt-4 mx-3 mx-md-0">
               <h1 className="mb-0 fw-bold">Overview</h1>
               <hr style={{ height: "2px" }} className="mt-2" />
               <div className="row">
-                <div className="col-9">
+                <div className="col-12 col-md-9 mb-3">
                   <div className="row d-flex align-items-center">
-                    <div className="col-8 overview-line">
+                    <div className="col-12 col-md-8 overview-line">
                       {parse(config[pf.Id])}
                     </div>
-                    <div className="col-4">
+                    <div className="col-12 col-md-4">
                       <p className="d-block fw-bold mb-1">Factsheet</p>
                       <p className="d-block fw-bold">
                         Download key points about this IAP
@@ -143,7 +143,7 @@ export function SinglePortFolio({ match }) {
                     </div>
                   </div>
                 </div>
-                <div className="col-3">
+                <div className="col-12 col-md-3">
                   <button
                     className="btn btn-dark mb-2"
                     style={{ width: "100%" }}
@@ -159,7 +159,7 @@ export function SinglePortFolio({ match }) {
           </div>
           <div className="past_performance_section">
             <div className="mt-4">
-              <h1 className="mb-0 fw-bold">
+              <h1 className="mb-0 fw-bold mx-3 mx-md-0">
                 Past Performanc vs Nifty 100 Index
               </h1>
             </div>
@@ -176,8 +176,8 @@ export function SinglePortFolio({ match }) {
             </div>
             {navs.length !== 0 && (
               <Fragment>
-                <div className="d-flex justify-content-between">
-                  <div>
+                <div className="d-flex flex-column flex-md-row justify-content-between mx-3 mx-md-3">
+                  <div className="mb-3">
                     <h1>
                       {pf.ModelName} vs {navs && navs[0].BenchMarkName}
                     </h1>
@@ -235,7 +235,7 @@ export function SinglePortFolio({ match }) {
                       </div>
                     </div>
                   </div>
-                  <div>
+                  <div className="mb-3">
                     <p className="text-center fw-bold mx-5 mb-2 px-2">
                       Rs. 1 Cr invested would be
                     </p>
@@ -322,86 +322,88 @@ export function SinglePortFolio({ match }) {
                     </div>
                   </div>
                 </div>
-                <Line
-                  options={{
-                    elements: {
-                      point: {
-                        radius: 0,
-                      },
-                    },
-                    plugins: {
-                      legend: {
-                        position: "top-right",
-                      },
-                    },
-                    scales: {
-                      x: {
-                        type: "time",
-                        time: {
-                          unit:
-                            navTime === "3y" ||
-                            navTime === "5y" ||
-                            navTime === "max"
-                              ? "year"
-                              : "month",
+                <div className="mx-3">
+                  <Line
+                    options={{
+                      elements: {
+                        point: {
+                          radius: 0,
                         },
                       },
-                    },
-                  }}
-                  data={{
-                    // labels: ["2017", "2018", "2019", "2020", "2021"],
-                    datasets: [
-                      {
-                        label: pf.ModelName,
-                        data: navs.map((n) => ({
-                          x: n.navDate,
-                          y: n.navValue,
-                        })),
-                        backgroundColor: ["rgb(50, 36, 57)"],
-                        borderColor: ["rgb(50, 36, 57)"],
-                        borderWidth: 4,
+                      plugins: {
+                        legend: {
+                          position: "top-right",
+                        },
                       },
-                      {
-                        label: navs && navs[0].BenchMarkName,
-                        data: navs.map((n) => ({
-                          x: n.navDate,
-                          y: n.BenchmarkNAV,
-                        })),
-                        backgroundColor: ["rgb(196, 196, 196)"],
-                        borderColor: ["rgb(196, 196, 196)"],
-                        borderWidth: 4,
+                      scales: {
+                        x: {
+                          type: "time",
+                          time: {
+                            unit:
+                              navTime === "3y" ||
+                              navTime === "5y" ||
+                              navTime === "max"
+                                ? "year"
+                                : "month",
+                          },
+                        },
                       },
-                    ],
-                  }}
-                />
+                    }}
+                    data={{
+                      // labels: ["2017", "2018", "2019", "2020", "2021"],
+                      datasets: [
+                        {
+                          label: pf.ModelName,
+                          data: navs.map((n) => ({
+                            x: n.navDate,
+                            y: n.navValue,
+                          })),
+                          backgroundColor: ["rgb(50, 36, 57)"],
+                          borderColor: ["rgb(50, 36, 57)"],
+                          borderWidth: 4,
+                        },
+                        {
+                          label: navs && navs[0].BenchMarkName,
+                          data: navs.map((n) => ({
+                            x: n.navDate,
+                            y: n.BenchmarkNAV,
+                          })),
+                          backgroundColor: ["rgb(196, 196, 196)"],
+                          borderColor: ["rgb(196, 196, 196)"],
+                          borderWidth: 4,
+                        },
+                      ],
+                    }}
+                  />
+                </div>
               </Fragment>
             )}
-            <small>
+            <small className="d-block mx-3 my-3">
               Note: Past performance graph includes changes due to rebalance,
               events like stock splits &amp; mergers. Also, past performance
               doesn't guarantee future returns
             </small>
-            <div className="values">
+            <div className="values mx-3">
               <h1 className="bg-dark text-white text-center mt-2 py-2">
                 Value of INR 1 Crore Invested In
               </h1>
               <h1
-                className="text-center py-2 d-flex justify-content-evenly"
+                className="text-center py-2 d-flex flex-column flex-md-row justify-content-evenly"
                 style={{ borderBottom: "1px solid #1D3750" }}
               >
                 <div>NS Industry Leaders - Rs. 1.57 Crore</div>
-                <div className="vsLine"></div>
+                <div className="vsLine d-none d-md-block"></div>
                 <div>Vs</div>
-                <div className="vsLine"></div>
+                <div className="vsLine d-none d-md-block"></div>
                 <div>Nifty 100 Index - Rs. 1.55 Crore</div>
               </h1>
             </div>
           </div>
-          <div className="port_alloc">
+          <div className="port_alloc mx-3">
             <div className="mt-4">
               <h1 className="mb-0 fw-bold">Portfolio Allocation</h1>
               <div className="row">
-                <div className="col-6">
+                <div className="col-12 col-md-6">
                   <h1
                     className="text-center fw-bold py-2"
                     style={{ backgroundColor: "#F1F2F2" }}
@@ -443,7 +445,7 @@ export function SinglePortFolio({ match }) {
                     }}
                   />
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-md-6">
                   <h1
                     className="text-center fw-bold py-2"
                     style={{ backgroundColor: "#F1F2F2" }}
